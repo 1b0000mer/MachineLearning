e1619339c6125f57f6a505ba2fb65f4e34bf4782
Nguyễn Thiên Quốc <thienquoc98@gmail.com>
2019-05-14 17:22:37 +0700

    update MLR
    have plot data & prediction now

--- MachineLearning/Multivariate_LinearRegression.py
+++ MachineLearning/Multivariate_LinearRegression.py
@@ -1,19 +1,41 @@
-import numpy as np
-from func_LR import computeCost, gradientDescent
 import os           #for getting file path
+import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-filePath = os.getcwd() + '\multiLR\data2.txt'
+from func_LR import computeCost, gradientDescent
+
+###input
+filePath = os.getcwd() + '\multiLR\data.txt'
 feature = ['Size', 'Room', 'Price']
 training_data = pd.read_csv(filePath, names=feature)
 
+#for plot
+x_pop = np.linspace(training_data.values[:,0].min(), training_data.values[:,0].max(), 100,dtype=int)
+y_pop = np.linspace(training_data.values[:,1].min(), training_data.values[:,1].max(), 100,dtype=int)
+######
+
 print(training_data.shape)
 print(training_data.head())
-print('#####################')
-#training_data.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-#plt.pyplot.show()
+print('######################')
+#training_data.plot()
+#plt.show()
 
+fig1 = plt.figure()
+ax = plt.axes(projection='3d')
+
+#Data for three-dimensional scattered points
+zdata = training_data.values[:,2]
+xdata = training_data.values[:,0]
+ydata = training_data.values[:,1]
+ax.set_xlabel('Size')
+ax.set_ylabel('Room')
+ax.set_zlabel('Price')
+ax.set_title('Training Data')
+ax.scatter3D(xdata, ydata, zdata);
+
+#Standardization
 training_data = (training_data - training_data.mean())/training_data.std()
 
 matrix = training_data.values
@@ -30,13 +52,35 @@ theta = np.zeros((1, X.shape[1]))
 
 #print(computeCost(X, y, theta))
 
+###begin
+loop = 1000
 theta_final, J_cost = gradientDescent(X, y, theta, m, loop=1000)
-print(theta_final)
-print(J_cost[-1]) #min
+#########
 
-#guess
-size = 1100.0
+print('Theta:')
+print(theta_final)
+print('cost func: %f' %J_cost[-1])
+#print(J_cost[-1]) #min
+
+###theta prediction fig
+#ax.scatter3D(x_pop, y_pop, f, 'r', label='Prediction')
+f = ((theta_final[0, 0] * x_pop) + (theta_final[0, 1] * y_pop) + theta_final[0, 2])*100000
+ax.plot3D(x_pop, y_pop, f, 'green', label='prediction');
+########
+
+###guess
+print('######################')
+size = 2000.0
 room = 3.0
 test = ([size, room, 1.] * theta_final)
 print('guest for: size=%d, room=%d' %(size, room))
-print(test.sum(axis = 1))
+print('price: %f' %(test.sum(axis = 1)*100000))
+
+###cost func fig
+fig2 = plt.figure()
+cs = plt.axes()
+cs.plot(np.arange(loop), J_cost, 'r')
+cs.set_xlabel('So Lan Lap')
+cs.set_ylabel('Ham Chi Phi')
+cs.set_title('Ham Chi Phi vs. So Lan Lap')
+plt.show()
--- MachineLearning/func_LR.py
+++ MachineLearning/func_LR.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
 def h_X(X, theta):
     return np.dot(X, theta.T)
@@ -11,21 +10,23 @@ def computeCost(X, y, theta, m):
     return J
 
 def gradientDescent(X, y, theta, m, alpha = 0.01, loop = 10):
-    theta_center = np.zeros(theta.shape)
+    '''
+    X: data input không có cột cuối (Price)
+    y: cột cuối (Price)
+    m: số lượng data input
+    alpha: learning rate
+    loop: số lần lặp của thuật toán
+    '''
+    theta_temp = np.zeros(theta.shape)
     var_Theta = theta.shape[1]
-    J_center = np.zeros(loop)
+    J_temp = np.zeros(loop)
     for i in range(loop):
         err = h_X(X,theta) - y
         for j in range(var_Theta):
             x_ij = np.reshape(X[:,j],(len(X),1))
             term = np.multiply(err,x_ij)
-            theta_center[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
-        theta = theta_center
-        J_center[i] = computeCost(X,y,theta,m)
+            theta_temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
+        theta = theta_temp
+        J_temp[i] = computeCost(X,y,theta,m)
     
-    return theta, J_center
-
-#def plotfig():
-#    x_pop = np.linspace(X_dacTinh.min(), X_dacTinh.max(), 100)
-#    f = theta_final[0, 0] + (theta_final[0, 1] * x_pop)
-#    fig, ax = plt.subplots() # (figsize=(12,8))
+    return theta, J_temp
